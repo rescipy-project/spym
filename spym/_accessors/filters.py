114ffeb2ef372c8705fb84f0231cfde698bd940a
mpanighel <mirkopanighel@gmail.com>
2020-03-18 18:10:05 +0100

    Add in_place option to processing methods.

--- spym/_accessors/filters.py
+++ spym/_accessors/filters.py
@@ -8,59 +8,81 @@ class SpymFilters():
     def __init__(self, spym_instance):
         self._spym = spym_instance
 
-    def gauss(self, **kwargs):
-        ''' Apply Gaussian smoothing filter
+    def gauss(self, in_place=True, **kwargs):
+        ''' Apply Gaussian smoothing filter.
 
         Args:
-            size: size of the filter in pixels
+            size: size of the filter in pixels.
+            in_place: if True the function is applied to the DataArray (default),
+                if False a new DataArray is returned.
         '''
 
-        self._spym._dr.data = spym_filters.gauss(self._spym._dr.data, **kwargs)
+        if in_place:
+            self._spym._dr.data = spym_filters.gauss(self._spym._dr.data, **kwargs)
+        else:
+            return self._spym._dr.copy(deep=True, data=spym_filters.gauss(self._spym._dr.data, **kwargs))
 
-    def median(self, **kwargs):
-        ''' Apply median smoothing filter
+    def median(self, in_place=True, **kwargs):
+        ''' Apply median smoothing filter.
 
         Args:
-            size: size of the filter in pixels
+            size: size of the filter in pixels.
+            in_place: if True the function is applied to the DataArray (default),
+                if False a new DataArray is returned.
         '''
 
-        self._spym._dr.data = spym_filters.median(self._spym._dr.data, **kwargs)
+        if in_place:
+            self._spym._dr.data = spym_filters.median(self._spym._dr.data, **kwargs)
+        else:
+            return self._spym._dr.copy(deep=True, data=spym_filters.median(self._spym._dr.data, **kwargs))
 
-    def mean(self, **kwargs):
-        ''' Apply mean smoothing filter
+    def mean(self, in_place=True, **kwargs):
+        ''' Apply mean smoothing filter.
 
         Args:
-            size: size of the filter in pixels
+            size: size of the filter in pixels.
+            in_place: if True the function is applied to the DataArray (default),
+                if False a new DataArray is returned.
         '''
 
-        self._spym._dr.data = spym_filters.mean(self._spym._dr.data, **kwargs)
+        if in_place:
+            self._spym._dr.data = spym_filters.mean(self._spym._dr.data, **kwargs)
+        else:
+            return self._spym._dr.copy(deep=True, data=spym_filters.mean(self._spym._dr.data, **kwargs))
 
-    def sharpen(self, **kwargs):
-        ''' Apply a sharpening filter
+    def sharpen(self, in_place=True, **kwargs):
+        ''' Apply a sharpening filter.
 
         Args:
-            size: size of the filter in pixels
-            alpha: weight
+            size: size of the filter in pixels.
+            alpha: weight.
+            in_place: if True the function is applied to the DataArray (default),
+                if False a new DataArray is returned.
         '''
 
-        self._spym._dr.data = spym_filters.sharpen(self._spym._dr.data, **kwargs)
+        if in_place:
+            self._spym._dr.data = spym_filters.sharpen(self._spym._dr.data, **kwargs)
+        else:
+            return self._spym._dr.copy(deep=True, data=spym_filters.sharpen(self._spym._dr.data, **kwargs))
 
-    def destripe(self, **kwargs):
-        ''' Find and remove scan stripes by averaging neighbourhood lines
+    def destripe(self, in_place=True, **kwargs):
+        ''' Find and remove scan stripes by averaging neighbourhood lines.
 
         Args:
-            image: 2d numpy array
-            min_lenght: only scars that are as long or longer than this value (in pixels) will be marked
+            min_lenght: only scars that are as long or longer than this value (in pixels) will be marked.
             hard_threshold: the minimum difference of the value from the neighbouring upper and lower lines to be considered a defect.
             soft_threshold: values differing at least this much do not form defects themselves,
                 but they are attached to defects obtained from the hard threshold if they touch one.
             only_mask: wheter returning just the mask (True) or also the corrected data (False, default).
-
-        Returns:
-            destriped 2d array
+            in_place: if True the function is applied to the DataArray (default),
+                if False a new DataArray is returned.
         '''
 
         if not self._spym._dr.data.ndim == 2:
             print("The DataArray is not an image. Abort.")
             return
-        self._spym._dr.data, self._spym._mask = spym_filters.destripe(self._spym._dr.data.astype(float), **kwargs)
+
+        if in_place:
+            self._spym._dr.data, self._spym._mask = spym_filters.destripe(self._spym._dr.data.astype(float), **kwargs)
+        else:
+            return self._spym._dr.copy(deep=True, data=spym_filters.destripe(self._spym._dr.data.astype(float), **kwargs)[0])
--- spym/_accessors/level.py
+++ spym/_accessors/level.py
@@ -8,35 +8,53 @@ class SpymLevel():
     def __init__(self, spym_instance):
         self._spym = spym_instance
 
-    def fixzero(self, **kwargs):
+    def fixzero(self, in_place=True, **kwargs):
         ''' Add a constant to all the data to move the minimum (or the mean value) to zero.
         
         Args:
             to_mean: bool, optional. If true move mean value to zero, if false move mimimum to zero (default).
+            in_place: if True the function is applied to the DataArray (default),
+                if False a new DataArray is returned.
         '''
 
-        self._spym._dr.data = spym_level.fixzero(self._spym._dr.data, **kwargs)
+        if in_place:
+            self._spym._dr.data = spym_level.fixzero(self._spym._dr.data, **kwargs)
+        else:
+            return self._spym._dr.copy(deep=True, data=spym_level.fixzero(self._spym._dr.data, **kwargs))
 
-    def plane(self, **kwargs):
+    def plane(self, in_place=True, **kwargs):
         '''Corrects for sample tilting by subtraction of a plane.
         
+        Args:
+            in_place: if True the function is applied to the DataArray (default),
+                if False a new DataArray is returned.
         '''
 
         if not self._spym._dr.data.ndim == 2:
             print("The DataArray is not an image. Abort.")
             return
-        self._spym._dr.data, self._spym._bkg = spym_level.plane(self._spym._dr.data.astype(float), **kwargs)
 
-    def align(self, **kwargs):
+        if in_place:
+            self._spym._dr.data, self._spym._bkg = spym_level.plane(self._spym._dr.data.astype(float), **kwargs)
+        else:
+            return self._spym._dr.copy(deep=True, data=spym_level.plane(self._spym._dr.data.astype(float), **kwargs)[0])
+
+    def align(self, in_place=True, **kwargs):
         '''Align rows.
         
         Args:
             baseline: defines how baselines are estimated; 'median' (default), 'mean', 'poly'.
             axis: axis along wich calculate the baselines.
             poly_degree: polnomial degree if baseline='poly'.
+            in_place: if True the function is applied to the DataArray (default),
+                if False a new DataArray is returned.
         '''
 
         if not self._spym._dr.data.ndim == 2:
             print("The DataArray is not an image. Abort.")
             return
-        self._spym._dr.data, self._spym._bkg = spym_level.align(self._spym._dr.data.astype(float), **kwargs)
+
+        if in_place:
+            self._spym._dr.data, self._spym._bkg = spym_level.align(self._spym._dr.data.astype(float), **kwargs)
+        else:
+            return self._spym._dr.copy(deep=True, data=spym_level.align(self._spym._dr.data.astype(float), **kwargs)[0])
