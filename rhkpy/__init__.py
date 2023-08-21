2a19b36bf7999098823d7fa035f65c282b85741f
zrbyte <zrbyte@users.noreply.github.com>
2023-08-21 12:30:17 +0200

    added hvplot initialization to __init__

--- rhkpy/__init__.py
+++ rhkpy/__init__.py
@@ -2,3 +2,9 @@ from .rhkpy_loader import *
 from .rhkpy_process import *
 
 hvplot.extension('bokeh')
+from holoviews import opts
+opts.defaults(
+    opts.Image(
+        aspect = 1
+        )
+    ) # can't set default colormap with this
